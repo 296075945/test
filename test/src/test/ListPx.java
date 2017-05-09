49e7ba7d2db590795061b12ad5246f01b8a01228
weiyang <296075945@qq.com>
2017-05-09 08:53:17 +0800

    ..

--- test/src/test/ListPx.java
+++ test/src/test/ListPx.java
@@ -1,19 +1,55 @@
 package test;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class ListPx {
 
 	public static void main(String[] args) {
-		System.out.println();
+		List<A> list = new ArrayList<A>();  
+        
+        //创建3个学生对象，年龄分别是20、19、21，并将他们依次放入List中  
+        A a = new A();  
+        a.setA("aaa");
+        a.setB("ccc");
+        list.add(a);
+        a = new A();  
+        a.setA("bbb");
+        a.setB("bbb");
+        list.add(a);
+        a = new A();  
+        a.setA("aaa");
+        a.setB("ccc");
+        list.add(a);
+        Collections.sort(list,new Comparator<A>(){
+
+			@Override
+			public int compare(A arg0, A arg1) {
+				// TODO Auto-generated method stub
+				return 0;
+			}
+        	
+        });
+        
 	}
 }
-class A implements Comparable<A>{
+class A {
 
-	@Override
-	public int compareTo(A o) {
-		// TODO Auto-generated method stub
-		return 0;
+	private String A;
+	private String B;
+	public String getA() {
+		return A;
+	}
+	public void setA(String a) {
+		A = a;
+	}
+	public String getB() {
+		return B;
+	}
+	public void setB(String b) {
+		B = b;
 	}
 
 	
